6d98b4c88dda5cec9f21099ec8d4ab7d64f360e0
thenikhilsingh <nikhilsingh26042004@gmail.com>
2026-06-12 14:52:02 +0530

    user registeration and password hashing logic done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User.js");
+// const bcrypt = require("bcryptjs");
 
 const home = async (req, res) => {
   try {
@@ -13,22 +14,28 @@ const register = async (req, res) => {
   try {
     // 1. Get Registration Data: 📥 Retrieve user data (username, email, password).
     const { fullName, email, password } = req.body;
+
     // 2. Check Email Existence: 📋 Check if the email is already registered.
     const userExist = await User.findOne({ email: email }); // email in collection of db : email in body
     if (userExist) {
       return res.status(400).json({ message: "Email already exists!" });
     }
-    const userCreated = await User.create({ fullName, email, password });
-    res.status(200).json({ msg: userCreated });
-    // 3. Hash Password: 🔒 Securely hash the password.
+
+    // 3. Hash Password: 🔒 Securely hash the password. //we did this step in user model using "pre" method
+    // const saltRound = 10;
+    // const hash_password = bcrypt.hash(password, saltRound);
 
     // 4. Create User: 📝 Create a new user with hashed password.
-
-    // 5. Save to DB: 💾 Save user data to the database.
-
-    // 6. Respond: ✅ Respond with "Registration Successful" or handle errors.
+    const userCreated = await User.create({
+      fullName,
+      email,
+      // password: hash_password,
+      password,
+    });
+    res.status(201).json({ msg: userCreated });
+    
   } catch (error) {
-    res.status(400).send({ msg: "Internal Server Error" });
+    res.status(500).send({ msg: "Internal Server Error" });
   }
 };
 
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -23,5 +24,19 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   },
 );
+
+userSchema.pre("save", async function (next) {
+  const user = this; //this keyword stores the user data that is going to be created before created as it is in pre
+  if (!user.isModified("password")) {
+    next();
+  }
+  try {
+    const saltRound = await bcrypt.genSalt();
+    const hash_password = await bcrypt.hash(user.password, saltRound);
+    user.password = hash_password;
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = mongoose.model("User", userSchema);
